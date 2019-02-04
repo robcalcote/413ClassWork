fdac6da403382e080f0cb5ca91ceda4a9b73914e
Rob Calcote <robcalcote@gmail.com>
2019-02-03 22:42:11 -0700

    changed maintenance message styling and made minor edits to the home page

--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1549258072.5173953
+_modified_time = 1549258695.0932596
 _enable_loop = True
 _template_filename = 'C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -19,33 +19,35 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        def site_right():
-            return render_site_right(context._locals(__M_locals))
-        def site_center():
-            return render_site_center(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
         def site_left():
             return render_site_left(context._locals(__M_locals))
+        def site_right():
+            return render_site_right(context._locals(__M_locals))
         def title():
             return render_title(context._locals(__M_locals))
+        self = context.get('self', UNDEFINED)
         STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def site_center():
+            return render_site_center(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n        <title>Jedi &mdash; ')
+        __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n')
+        __M_writer('        <title>Test - ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
             context['self'].title(**pageargs)
         
 
-        __M_writer('</title>\r\n\r\n')
-        __M_writer('        \r\n\r\n')
+        __M_writer('</title>\r\n        <link rel="icon" href="')
+        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
+        __M_writer('homepage/media/pythonicon.png">  \r\n\r\n')
         __M_writer('        <script src="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
-        __M_writer('homepage/media/jquery-3.3.1.js"></script>\r\n')
+        __M_writer('homepage/media/jquery-3.3.1.js"></script>\r\n\r\n')
         __M_writer('        <link rel="stylesheet" href="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
         __M_writer('homepage/media/bootstrap-4.0.0-dist/css/bootstrap.min.css"/>\r\n        <script src="bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>\r\n        \r\n')
         __M_writer('        <script src="/django_mako_plus/dmp-common.min.js"></script>\r\n        ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( django_mako_plus.links(self) ))
-        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <div id="maintenance_message" class="alert alert-danger">\r\n            Hey There. The server is going down right now. Boo Hoo.\r\n        </div>\r\n\r\n        <header>\r\n            <img src="')
+        __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n\r\n        <div id="maintenance_message" class="alert alert-danger">\r\n            This page is under construction...\r\n        </div>\r\n\r\n        <header>\r\n            <img src="')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
         __M_writer('homepage/media/python.png" alt="python" />\r\n            <div class="title">Welcome to <br/> DMP!</div>\r\n        </header>\r\n\r\n        <main>\r\n            <div id="site_left">\r\n                ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'site_left'):
@@ -117,6 +119,6 @@ def render_site_right(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "33": 2, "38": 7, "39": 12, "40": 15, "41": 15, "42": 15, "43": 17, "44": 17, "45": 17, "46": 21, "47": 22, "48": 22, "49": 32, "50": 32, "55": 40, "60": 45, "65": 50, "71": 7, "82": 38, "88": 38, "94": 43, "100": 43, "106": 48, "112": 48, "118": 112}}
+{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 0, "33": 2, "34": 8, "39": 8, "40": 9, "41": 9, "42": 12, "43": 12, "44": 12, "45": 15, "46": 15, "47": 15, "48": 19, "49": 20, "50": 20, "51": 30, "52": 30, "57": 38, "62": 43, "67": 48, "73": 8, "84": 36, "90": 36, "96": 41, "102": 41, "108": 46, "114": 46, "120": 114}}
 __M_END_METADATA
 """
