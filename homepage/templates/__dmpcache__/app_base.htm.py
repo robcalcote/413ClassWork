8bbd40e41a0ec6af5eb2de6f8e2fdb32c092d471
Rob Calcote <robcalcote@gmail.com>
2019-02-08 15:58:33 -0700

    recoded img icon so that auto grader will read it... hopefully

--- homepage/templates/__dmpcache__/app_base.htm.py
+++ homepage/templates/__dmpcache__/app_base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1549511349.1616285
+_modified_time = 1549666493.3227391
 _enable_loop = True
 _template_filename = 'C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/app_base.htm'
 _template_uri = 'app_base.htm'
@@ -32,13 +32,12 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def left_column():
             return render_left_column(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
-        request = context.get('request', UNDEFINED)
-        def navbar_main():
-            return render_navbar_main(context._locals(__M_locals))
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def navbar_login():
             return render_navbar_login(context._locals(__M_locals))
+        def navbar_main():
+            return render_navbar_main(context._locals(__M_locals))
+        request = context.get('request', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'navbar_main'):
@@ -64,15 +63,12 @@ def render_body(context,**pageargs):
 def render_navbar_main(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        request = context.get('request', UNDEFINED)
         def navbar_main():
             return render_navbar_main(context)
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        request = context.get('request', UNDEFINED)
         self = context.get('self', UNDEFINED)
         __M_writer = context.writer()
-        __M_writer('\r\n    <li class="nav-item"><img class="site_icon" src="')
-        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
-        __M_writer('homepage/media/django.png"></a></li>\r\n    <li class="nav-item ')
+        __M_writer('\r\n    <li class="nav-item ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( 'active' if request.dmp.page == 'index' else '' ))
         __M_writer('">\r\n        <a class="nav-link" href="/">Home</a></li>\r\n    <li class="nav-item ')
         __M_writer(django_mako_plus.ExpressionPostProcessor(self)( 'active' if request.dmp.page == 'about' else '' ))
@@ -118,6 +114,6 @@ def render_left_column(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/app_base.htm", "uri": "app_base.htm", "source_encoding": "utf-8", "line_map": {"29": 0, "43": 1, "48": 13, "53": 24, "58": 33, "64": 5, "73": 5, "74": 6, "75": 6, "76": 7, "77": 7, "78": 9, "79": 9, "80": 11, "81": 11, "87": 15, "93": 15, "99": 26, "107": 26, "108": 27, "109": 27, "110": 29, "111": 29, "112": 31, "113": 31, "119": 113}}
+{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/app_base.htm", "uri": "app_base.htm", "source_encoding": "utf-8", "line_map": {"29": 0, "42": 1, "47": 12, "52": 23, "57": 32, "63": 5, "71": 5, "72": 6, "73": 6, "74": 8, "75": 8, "76": 10, "77": 10, "83": 14, "89": 14, "95": 25, "103": 25, "104": 26, "105": 26, "106": 28, "107": 28, "108": 30, "109": 30, "115": 109}}
 __M_END_METADATA
 """
--- homepage/templates/__dmpcache__/base.htm.py
+++ homepage/templates/__dmpcache__/base.htm.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1549513233.5690691
+_modified_time = 1549666486.369681
 _enable_loop = True
 _template_filename = 'C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm'
 _template_uri = 'base.htm'
@@ -23,20 +23,20 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def title():
             return render_title(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def site_title():
+            return render_site_title(context._locals(__M_locals))
+        def left_column():
+            return render_left_column(context._locals(__M_locals))
+        def navbar_login():
+            return render_navbar_login(context._locals(__M_locals))
+        def center_content():
+            return render_center_content(context._locals(__M_locals))
+        def navbar_main():
+            return render_navbar_main(context._locals(__M_locals))
         def right_column():
             return render_right_column(context._locals(__M_locals))
         self = context.get('self', UNDEFINED)
-        def left_column():
-            return render_left_column(context._locals(__M_locals))
-        def site_title():
-            return render_site_title(context._locals(__M_locals))
-        def navbar_main():
-            return render_navbar_main(context._locals(__M_locals))
-        def navbar_login():
-            return render_navbar_login(context._locals(__M_locals))
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
-        def center_content():
-            return render_center_content(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('<!DOCTYPE html>\r\n<html>\r\n    <meta charset="UTF-8">\r\n    <head>\r\n\r\n')
         __M_writer('        <title>Sprint 1 - ')
@@ -60,7 +60,9 @@ def render_body(context,**pageargs):
         __M_writer('\r\n\r\n    </head>\r\n    <body>\r\n')
         __M_writer('        <div id="maintenance_message" class="alert alert-danger">\r\n            This page is under construction...\r\n        </div>\r\n\r\n        <br /><br /><br />\r\n        <header>\r\n')
         __M_writer('            <div id="navbar_main">\r\n                <nav class="navbar navbar-expand-lg navbar-light bg-light">\r\n')
-        __M_writer('                    <div class="collapse navbar-collapse" id="navbarSupportedContent">\r\n                        <ul class="navbar-nav mr-auto">\r\n                            ')
+        __M_writer('                    <div class="collapse navbar-collapse" id="navbarSupportedContent">\r\n                        <ul class="navbar-nav mr-auto">\r\n                            <li class="nav-item"><img class="site_icon" src="')
+        __M_writer(django_mako_plus.ExpressionPostProcessor(self)( STATIC_URL ))
+        __M_writer('homepage/media/django.png"></a></li>\r\n                            ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'navbar_main'):
             context['self'].navbar_main(**pageargs)
         
@@ -181,6 +183,6 @@ def render_right_column(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 70, "20": 0, "41": 2, "42": 8, "47": 8, "48": 9, "49": 9, "50": 12, "51": 12, "52": 12, "53": 15, "54": 15, "55": 15, "56": 16, "57": 16, "58": 19, "59": 20, "60": 20, "61": 25, "62": 32, "63": 35, "68": 37, "73": 40, "74": 49, "79": 50, "80": 54, "81": 56, "86": 56, "91": 59, "96": 62, "97": 70, "98": 71, "99": 71, "105": 8, "116": 37, "127": 40, "138": 50, "149": 56, "160": 59, "171": 62, "182": 171}}
+{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "utf-8", "line_map": {"18": 71, "20": 0, "41": 2, "42": 8, "47": 8, "48": 9, "49": 9, "50": 12, "51": 12, "52": 12, "53": 15, "54": 15, "55": 15, "56": 16, "57": 16, "58": 19, "59": 20, "60": 20, "61": 25, "62": 32, "63": 35, "64": 37, "65": 37, "70": 38, "75": 41, "76": 50, "81": 51, "82": 55, "83": 57, "88": 57, "93": 60, "98": 63, "99": 71, "100": 72, "101": 72, "107": 8, "118": 38, "129": 41, "140": 51, "151": 57, "162": 60, "173": 63, "184": 173}}
 __M_END_METADATA
 """
