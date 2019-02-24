a9a44553190064f82bace9ec5bbadddfd3f96e7d
Rob Calcote <robcalcote@gmail.com>
2019-02-23 17:06:33 -0700

    UnitTesting stuff - Testing DB and fixtures test file

--- account/tests.py
+++ account/tests.py
@@ -1,19 +1,30 @@
 from django.test import TestCase
 from account import models as amod
 
-class AccounTests(TestCase):
+class AccountTests(TestCase):
     fixtures = [ 'account.yaml' ]
 
     def test_user_get(self):
         u1 = amod.User.objects.get(id=1)
         # We are testing that the user with id=1 has the first name Homer
-        self.assertEqual(u1.first_name, 'Hom2er', msg="Name should have been 'Homer'")
+        self.assertEqual(u1.first_name, 'Homer', msg="Name should have been 'Homer'")
 
     def test_user_create(self):
         u = amod.User()
+        u.username = 'harrypotter'
         u.first_name = 'Harry'
         u.last_name = 'Potter'
-        u........
+        u.set_password('password123')
+        u.is_superuser = False
+        u.email = 'harry@potter.com'
+        u.favorite_color = 'black'
         u.save()
-        u2 = aomd.User.objects.get(u.id)
-        self.assertEqual(u.first_name, u2.first_name, msg="...")
+        u4 = amod.User.objects.get(id=4)
+        self.assertEqual(u.first_name, u4.first_name, msg="Name should have been 'Harry'")
+
+class LoginTests(TestCase):
+    fixtures = [ '' ]
+
+class GroupPermissions(TestCase):
+    fixtures = [ '' ]
+
