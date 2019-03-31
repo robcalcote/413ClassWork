a4d1df5bb2cc6169c290bf840bb2d186d2fb34a5
Rob Calcote <robcalcote@gmail.com>
2019-03-31 14:56:35 -0600

    fixed image display in shopping cart

--- catalog/templates/__dmpcache__/cart.html.py
+++ catalog/templates/__dmpcache__/cart.html.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554063308.5705466
+_modified_time = 1554065674.8284392
 _enable_loop = True
 _template_filename = 'C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/catalog/templates/cart.html'
 _template_uri = 'cart.html'
@@ -31,12 +31,12 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         sale = context.get('sale', UNDEFINED)
+        cartItems = context.get('cartItems', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         def title():
             return render_title(context._locals(__M_locals))
         def center_content():
             return render_center_content(context._locals(__M_locals))
-        self = context.get('self', UNDEFINED)
-        cartItems = context.get('cartItems', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
@@ -68,16 +68,18 @@ def render_title(context,**pageargs):
 def render_center_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        cartItems = context.get('cartItems', UNDEFINED)
-        sale = context.get('sale', UNDEFINED)
+        self = context.get('self', UNDEFINED)
         def center_content():
             return render_center_content(context)
-        self = context.get('self', UNDEFINED)
+        sale = context.get('sale', UNDEFINED)
+        cartItems = context.get('cartItems', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n    <div id="shopping_cart">\r\n        <br />\r\n        <table>\r\n')
         __M_writer('            <tr>\r\n                <th>Product Image</th>\r\n                <th>Product Name</th>\r\n                <th>Quantity</th>\r\n                <th>Price</th>\r\n                <th>Extended</th>\r\n                <th>Actions</th>\r\n            </tr>\r\n')
         for si in cartItems:
-            __M_writer('                <tr>   \r\n                    <td></td>\r\n                    <td>')
+            __M_writer('                <tr>\r\n                    <td><img src="')
+            __M_writer(django_mako_plus.ExpressionPostProcessor(self)(si.product.image_url()))
+            __M_writer('" width="60px" height="60px"></td>\r\n                    <td>')
             __M_writer(django_mako_plus.ExpressionPostProcessor(self)( si.product.name ))
             __M_writer('</td>\r\n                    <td>')
             __M_writer(django_mako_plus.ExpressionPostProcessor(self)( si.quantity ))
@@ -102,6 +104,6 @@ def render_center_content(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/catalog/templates/cart.html", "uri": "cart.html", "source_encoding": "utf-8", "line_map": {"29": 0, "41": 1, "46": 4, "56": 4, "62": 4, "68": 7, "77": 7, "78": 12, "79": 22, "80": 23, "81": 25, "82": 25, "83": 26, "84": 26, "85": 27, "86": 27, "87": 28, "88": 28, "89": 29, "90": 29, "91": 32, "92": 38, "93": 38, "94": 45, "95": 45, "96": 54, "97": 54, "103": 97}}
+{"filename": "C:/Users/Rob/Desktop/BYU/Winter2019/413/project2class/catalog/templates/cart.html", "uri": "cart.html", "source_encoding": "utf-8", "line_map": {"29": 0, "41": 1, "46": 4, "56": 4, "62": 4, "68": 7, "77": 7, "78": 12, "79": 22, "80": 23, "81": 24, "82": 24, "83": 25, "84": 25, "85": 26, "86": 26, "87": 27, "88": 27, "89": 28, "90": 28, "91": 29, "92": 29, "93": 32, "94": 38, "95": 38, "96": 45, "97": 45, "98": 54, "99": 54, "105": 99}}
 __M_END_METADATA
 """
